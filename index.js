84757b51c21921d68ad881a9158bf9084d06f700
Adam Horvath <adam.horvath16299@gmail.com>
2023-01-25 18:53:39 +0100

    Add if/else statment to make sure input is less than 100

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 const mainContainer = document.querySelector('#container');
 mainContainer.classList.add('#container');
+
 const btn = document.querySelector('button');
 btn.addEventListener('click', function(e){
     let prValue = Number(prompt("Please enter number below 100"));
-    mainContainer.style.gridTemplateColumns = "repeat(" + prValue + ",1fr)";
-    mainContainer.style.gridTemplateRows = "repeat(" + prValue + ",1fr)";
-    for (let i = 0; i < prValue; i++){
-    for(let j = 0; j < prValue; j++){
-    const gridItems = document.createElement('div');
-    gridItems.classList.add('grid-item');
-    mainContainer.appendChild(gridItems);  
+    if (prValue >= 100){
+        alert('Number must be below 100, try again')
     }
-}
-const newItems = document.querySelectorAll('.grid-item');
-newItems.forEach(item => {
-    item.addEventListener('mouseenter', function() {
-        this.style.backgroundColor = 'white';
-    });
+    else{
+        mainContainer.style.gridTemplateColumns = "repeat(" + prValue + ",1fr)";
+        mainContainer.style.gridTemplateRows = "repeat(" + prValue + ",1fr)";
+        for (let i = 0; i < prValue; i++){
+        for(let j = 0; j < prValue; j++){
+        const gridItems = document.createElement('div');
+        gridItems.classList.add('grid-item');
+        mainContainer.appendChild(gridItems);  
+        };
+        };
 
-    item.addEventListener('mouseleave', function() {
-        this.style.backgroundColor = 'white';
-    });
-}); 
+        
+        const newItems = document.querySelectorAll('.grid-item');
+        newItems.forEach(item => {
+        item.addEventListener('mouseenter', function() {
+            this.style.backgroundColor = 'white';
+        });
+
+        item.addEventListener('mouseleave', function() {
+            this.style.backgroundColor = 'white';
+        });
+        }) 
+    }
 });
 
 
